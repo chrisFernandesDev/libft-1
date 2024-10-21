5deb28d8631d18266f281ba23095dd1ac02b791e
Tiago Alexandre Vasconcelos Rodrigues <tiagalex@c2r5s11.42porto.com>
2024-10-21 15:52:22 +0100

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,4 +1,46 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 14:52:35 by tiagalex          #+#    #+#             */
+/*   Updated: 2024/10/21 14:52:35 by tiagalex         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-//ainda na comecei
-void
+#include <stdio.h>
+#include <stddef.h>
+
+//copia um bloco de memoria
+void	*ft_memcpy(void *dest, const void *src, size_t len)
+{
+	unsigned char	*c_src;
+	unsigned char	*c_dest;
+	size_t	i;
+
+	c_dest = (unsigned char *)dest;
+	c_src = (unsigned char *)src;
+	i = 0;
+	if (c_src == NULL)
+	{
+		return (NULL);
+	}
+	while (len > i)
+	{
+		c_dest[i] = c_src[i];
+		i++;
+	}
+	return (dest);
+}
+/* 
+int	main()
+{
+	char	src[] = "alguma coisa";
+	char	dest[50];
+	ft_memcpy(dest, src, 20);
+
+	printf ("%s\n", dest);
+	return (0);
+} */
