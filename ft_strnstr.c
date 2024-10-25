f4926b8c2e382eca2c51c98403eab9496fc3eeca
Tiago Alexandre Vasconcelos Rodrigues <tiagalex@c2r5s11.42porto.com>
2024-10-25 14:44:39 +0100

    ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagalex <tiagalex@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/25 14:32:01 by tiagalex          #+#    #+#             */
+/*   Updated: 2024/10/25 14:43:46 by tiagalex         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//procura a agulha dentro do palheiro ate um certo limite
+char	*strnstr(const char *haystack, const char *needle, size_t len)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0' &&  i < len)
+	{
+		if (str[i] == needle)
+		{
+			/* code */
+		}
+		
+	}
+	
+}
